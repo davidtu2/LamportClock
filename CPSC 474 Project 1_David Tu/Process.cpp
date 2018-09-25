a36c77434e928d19b8147eabf7fa79d83d0f172a
David Tu <david.tu2@csu.fullerton.edu>
2018-09-25 16:55:47 -0700

    Added print functionality

---
+++ CPSC 474 Project 1_David Tu/Process.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Process.h"
+#include <iostream>
+using namespace std;
+
+//Constructor
+Process::Process(){
+
+}
+
+
+Process::~Process(){
+
+}
---
+++ CPSC 474 Project 1_David Tu/Process.h
@@ -0,0 +1,12 @@
+#pragma once
+class Process{
+
+public:
+	Process(string row[]) {
+
+	}
+
+	~Process();
+
+};
+
