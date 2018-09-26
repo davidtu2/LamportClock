1fc3cd4ad66045b3533bde4fdd23316f44c7f11c
David Tu <david.tu2@csu.fullerton.edu>
2018-09-25 18:55:37 -0700

    Added Process Class

--- CPSC 474 Project 1_David Tu/Process.cpp
+++ CPSC 474 Project 1_David Tu/Process.cpp
@@ -1,14 +1,30 @@
 #include "pch.h"
 #include "Process.h"
-#include <iostream>
-using namespace std;
 
-//Constructor
-Process::Process(){
+Process::Process(string row[], int cols){
+	noEvents = cols;
 
+	//Now that we know the number of events for this process, specify the size of the array
+	events = new string[noEvents];
+
+	for (int i = 0; i < noEvents; i++) {
+		if (row[i] != "NULL") {
+			events[i] = row[i];
+		}
+		else {
+			//Replace "NULL" entries with "0"
+			events[i] = "0";
+		}
+		
+	}
 }
 
-
 Process::~Process(){
+	delete[] events;
+}
 
+void Process::print() {
+	for (int i = 0; i < noEvents; i++) {
+		cout << "THE EVENT IS: " << events[i] << endl;
+	}
 }
--- CPSC 474 Project 1_David Tu/Process.h
+++ CPSC 474 Project 1_David Tu/Process.h
@@ -1,12 +1,17 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Process{
-
 public:
-	Process(string row[]) {
-
-	}
-
+	Process(string row[], int cols);
 	~Process();
 
+	void print();
+
+private:
+	int noEvents;
+	string *events;//Pointer
 };
 
