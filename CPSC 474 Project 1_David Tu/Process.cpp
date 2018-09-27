18a9e6dfd61e406187ac0adb91c7d8340e14c19d
David Tu <david.tu2@csu.fullerton.edu>
2018-09-26 17:23:08 -0700

    baseline

--- CPSC 474 Project 1_David Tu/Process.cpp
+++ CPSC 474 Project 1_David Tu/Process.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "Process.h"
 
-Process::Process(string row[], int cols, string name){
+Process::Process(const string row[], const int cols, const string name){
 	pName = name;
 	noEvents = cols;
 
@@ -30,7 +30,7 @@ Process::~Process(){
 }
 
 void Process::print() {
-	cout << "The events for process " << pName << " are as follows:" << endl;
+	cout << "The events for process " << pName << " are:" << endl;
 
 	for (int i = 0; i < noEvents; i++) {
 		//If I'm on the last entry, don't bother putting a "," at the end
@@ -42,11 +42,11 @@ void Process::print() {
 		}
 	}
 
-	cout << endl;
+	cout << "\n" << endl;
 }
 
 void Process::printSolution() {
-	cout << "The LC values for process " << pName << " are as follows:" << endl;
+	cout << "The LC values for process " << pName << " are:" << endl;
 	
 	for (int i = 0; i < noEvents; i++) {
 		if (i == noEvents - 1) {
@@ -57,5 +57,75 @@ void Process::printSolution() {
 		}
 	}
 	
-	cout << endl;
+	cout << "\n" << endl;
+}
+
+void Process::calculateLamport() {
+	for (int i = 0; i < noEvents; i++) {
+
+		//CASE 1: Internal events:
+		if (events[i].at(0) != 'r' && events[i].at(0) != 's') {
+			cout << "Processing internal event: " << events[i] << endl;
+			
+			//Internal event value is 1, so just add it to the prev LC value, K: K + 1
+			//If this is the first event, then there's no prev LC value, so make K = 0:
+			if (i == 0) {
+				output[i] = 1;
+			}
+			else {
+				output[i] = output[i - 1] + 1;
+			}
+		}
+
+		//CASE 2: Send events
+		else if (events[i].at(0) == 's') {
+			cout << "Processing send event: " << events[i] << endl;
+
+			//Send event value is 1, so just add to prev LC value, K: K + 1
+			//If this is the first event, then there's no prev LC value, so make K = 0:
+			if (i == 0) {
+				output[i] = 1;
+			}
+			else {
+				output[i] = output[i - 1] + 1;
+			}
+		}
+
+		//CASE 3: Receive events
+		else if (events[i].at(0) == 'r') {	
+			cout << "Processing receive event: " << events[i] << endl;
+
+			//Receive event value is determined by max(K, sendVal) + 1,
+			//where K is the prev LC value and sendVal is the value of the corresponding send event
+			//To perform this calculation, we first need the values of K and sendVal:
+			
+			//Obtain K:
+			int k;
+			
+			//If this is the first event, then there's no prev LC value, so make K = 0:
+			if (i == 0) {
+				k = 0;
+			}
+			else {
+				k = output[i - 1];
+			}
+
+			cout << "The prev LC value, K, is: " << k << endl;
+
+			//Now that we have K, obtain sendVal:
+			int sendVal;//TODO: get this from a shared variable
+			sendVal = 10;
+
+			cout << "Obtained sendVal value is: " << sendVal << endl;
+
+			//Finally calculate LC value:
+			output[i] = max(k, sendVal) + 1;
+		}
+		
+		else {
+			cout << "ERROR in calculateLamport()" << endl;
+		}
+
+		cout << "Result: " << output[i] << "\n" << endl;
+	}
 }
--- CPSC 474 Project 1_David Tu/Process.h
+++ CPSC 474 Project 1_David Tu/Process.h
@@ -1,15 +1,18 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Process{
 public:
-	Process(string row[], int cols, string name);
+	Process(const string row[], const int cols, const string name);
 	~Process();
 
 	void print();
 	void printSolution();
+	
+	void calculateLamport();
 
 private:
 	int noEvents;
